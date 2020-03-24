8683c41e34066b8f3467fd8472044910871b3fa2
Danny <danny20012011@icloud.com>
2020-03-24 14:42:07 -0400

    Added png files and started to work on js file, the start screen in finished

---
+++ rps.js
@@ -0,0 +1,18 @@
+const playerScore = 0;
+const computerScore = 0;
+const playerScore_span = document.getElementById("player-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreboard_section = document.querySelector(".game");
+const result_div = document.querySelector(".result")
+const rock_div = document.getElementById("r")
+const paper_div = document.getElementById("p")
+const scissors_div = document.getElementById("s")
+
+rock_div.addEventListener('click', function() {
+    console.log("Hey you clicked on Rock!");
+})
+
+
+
+
+
