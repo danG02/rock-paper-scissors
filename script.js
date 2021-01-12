a353bc0a49a091402899fe101ba4420b3352cb72
Danny <danny20012011@icloud.com>
2021-01-12 14:39:32 -0500

    Updated UI, new character design, made it easier to show who won

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const result_p = document.querySelector(".result > p")
 const rock_div = document.getElementById("r")
 const paper_div = document.getElementById("p")
 const scissors_div = document.getElementById("s")
+const winnerBox = document.querySelector('#winnerBox');
+
+
 
 const getComputerChoice = () => {
     const choices = ['r', 'p', 's'];
@@ -25,27 +28,37 @@ const win = (playerChoice, computerChoice) => {
     playerScore++;
     playerScore_span.innerHTML = playerScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${convertToWord(playerChoice)} beats ${convertToWord(computerChoice)}. You Win!`;
+    result_p.innerHTML = ` You picked ${convertToWord(playerChoice)}, ${convertToWord(playerChoice)} beats ${convertToWord(computerChoice)}. You Win!`;
     document.getElementById(playerChoice).classList.add('green-glow');
     setTimeout(function() { document.getElementById(playerChoice).classList.remove('green-glow') }, 300);
+    result_p.style.color = "green";
 }
 
 const lose = (playerChoice, computerChoice) => {
     computerScore++;
     playerScore_span.innerHTML = playerScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${convertToWord(playerChoice)} loses to ${convertToWord(computerChoice)}. You Lose...`;
+    result_p.innerHTML = ` You picked ${convertToWord(playerChoice)}, ${convertToWord(playerChoice)} loses to ${convertToWord(computerChoice)}. You Lose...`;
     document.getElementById(playerChoice).classList.add('red-glow');
     setTimeout(function() { document.getElementById(playerChoice).classList.remove('red-glow') }, 300);
+    result_p.style.color = "red";
 }
 
 const draw = (playerChoice, computerChoice) => {
     playerScore_span.innerHTML = playerScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${convertToWord(playerChoice)} and ${convertToWord(computerChoice)}. Its a Draw!`;
+    result_p.innerHTML = `You both picked ${convertToWord(playerChoice)} and ${convertToWord(computerChoice)}. Its a Draw!`;
     document.getElementById(playerChoice).classList.add('gray-glow');
     setTimeout(function() { document.getElementById(playerChoice).classList.remove('gray-glow') }, 300);
+    result_p.style.color = "yellow";
 }
+
+
+
+
+
+
+
 
 
 const game = playerChoice => {
